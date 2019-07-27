68b139a940dd75fab03bfcf3702b34b04cabae30
Reyan <reyanrahman@gmail.com>
2019-07-27 22:10:08 +0530

    Updates

---
+++ precache-manifest.81b788d797b23366320a4f231f04bd3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d15a042edc1573bff3a54c0c39cf751",
+    "url": "/newssnippets/static/media/logo.4d15a042.png"
+  },
+  {
+    "revision": "b534cadc146503b277a2",
+    "url": "/newssnippets/static/js/runtime~main.67cf3158.js"
+  },
+  {
+    "revision": "9266c99448dff2ecc185",
+    "url": "/newssnippets/static/js/main.a5936c17.chunk.js"
+  },
+  {
+    "revision": "4cc2ad93c21ded614fe7",
+    "url": "/newssnippets/static/js/2.25045221.chunk.js"
+  },
+  {
+    "revision": "9266c99448dff2ecc185",
+    "url": "/newssnippets/static/css/main.43482305.chunk.css"
+  },
+  {
+    "revision": "fba97ef367f0c911df99ca677008da21",
+    "url": "/newssnippets/index.html"
+  }
+];
